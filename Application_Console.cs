a78dee31ff269cc645895e98b4c343d22d0d9b44
Neil <62321203+CestMerNeil@users.noreply.github.com>
2023-01-02 18:09:23 +0100

    Création d'application

---
+++ Application_Console.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_APP_CONSOLE
+{
+    internal class Application_Console
+    {
+        static void Main(string[] args)
+        {
+            bool state = true;
+            Document_Management dm = new();
+            dm.CheckRoot();
+
+            Console.WriteLine("Hello, welcome to our system!");
+            while (state)
+            {
+                Console.WriteLine("Please input your operation.");
+                //Console.WriteLine("displayRoot: to get the path of root floder");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "hello":
+                        Console.WriteLine("hello");
+                        break;
+                    case "display":
+                        string adress = dm.GetPathRoot();
+                        Console.WriteLine(adress);
+                        Console.WriteLine(dm.GetTime(adress));
+                        break;
+                    case "addFloder":
+                        Console.WriteLine("Please enter the folder name:");
+                        string name = Console.ReadLine();
+                        dm.CreateFloder(name);
+                        break;
+                    case "exit":
+                        Console.WriteLine("Thank you for using this programme.");
+                        state = false;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown operator.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Document_Management.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace TP_APP_CONSOLE
+{
+    internal class Document_Management
+    {
+        private readonly string pathRoot = @"D:\\CodeGithub\\ISIMA_ZZ2\\NET\\TP_NOTE\\TP_APP_CONSOLE\\ROOT";
+
+        public string GetPathRoot()
+        {
+            return pathRoot;
+        }
+
+        /**
+          * @fn      checkRoot
+          * @brief   Check the root path and create it if it does not exist.
+          */
+        public void CheckRoot()
+        {
+            try
+            {
+                if (!Directory.Exists(pathRoot))
+                {
+                    Console.WriteLine("Creating the root directory.");
+                    Directory.CreateDirectory(pathRoot);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
+
+        /**
+          * @fn     createFloder
+          * @brief  Create a level 1 folder
+          */
+        public void CreateFloder(string name)
+        {
+            try
+            {
+                StringBuilder newFloder = new StringBuilder();
+                newFloder.Append(pathRoot)
+                    .Append("\\")
+                    .Append(name);
+                Directory.CreateDirectory(newFloder.ToString());
+                Console.WriteLine(newFloder.ToString() + "has been successfully created.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
+
+        public void AddContact(string name)
+        {
+            
+        }
+
+        public string GetTime(string path)
+        {
+            DateTime timeCreate = Directory.GetCreationTime(path);
+            DateTime timeMotive = Directory.GetLastWriteTime(path);
+            string time = "Creation time : " + timeCreate.ToString() +
+                ". Modify time : "  + timeMotive.ToString();
+            return time;
+        }
+    }
+}
